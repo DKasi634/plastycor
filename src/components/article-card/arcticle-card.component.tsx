d90e377082598d2acb9b298faf9c524b27fb3311
Daniel <dkasi634@gmail.com>
2025-01-31 14:48:00 +0300

    Modifying article card size

--- src/components/article-card/arcticle-card.component.tsx
+++ src/components/article-card/arcticle-card.component.tsx
@@ -11,7 +11,7 @@ const ArticleCard = ({ className, article: { image, title, description, publishe
         <div className={`${className} flex flex-col items-start justify-start p-[2px] rounded-lg border border-gray shadow-sm shadow-dark-transparent overflow-hidden pb-0` }>
             <div className="w-full h-[40%] mb-4 rounded-lg overflow-hidden"> <img src={image} alt="Article" className="w-full h-full object-cover object-center" /> </div>
 
-            <div className="flex flex-col items-center justify-start gap-3 p-4 -mb-12">
+            <div className="flex flex-col items-center justify-start gap-3 p-4 -mb-24 md:-mb-12">
                 <h3 className="text-lg font-semibold text-left w-full line-clamp-2 ">{title}</h3>
                 <p className="text-sm font-normal text-left w-full line-clamp-3">{description}</p>
 
