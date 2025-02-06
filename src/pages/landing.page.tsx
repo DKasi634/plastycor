fd0f42ea71e210640f44668540c51f0505526365
Daniel <dkasi634@gmail.com>
2025-02-06 18:58:10 +0300

    Adding testimonials sections

--- src/pages/landing.page.tsx
+++ src/pages/landing.page.tsx
@@ -5,7 +5,9 @@ import { CTASection } from '@/components/cta-section/cta-section.component'
 import HomeHero from '@/components/home-hero/home-hero.component'
 import ProductsSection from '@/components/product-section/product-section.component'
 import TeamSection from '@/components/team-section/team-section.component'
+import TestimonialsSection from '@/components/testimonials-section/testimonials-section.component'
 import { YouTubePlayer } from '@/components/youtube-player/youtube-player.component'
+import { productsTestimonials } from '@/constants/data'
 
 
 const LandingPage = () => {
@@ -18,7 +20,7 @@ const LandingPage = () => {
         <AchievementsSection />
         <ActivitiesSection />
         <ProductsSection/>
-        
+        <TestimonialsSection sectionLabel="Ce que disent nos clients" testimonials={productsTestimonials} />       
         <YouTubePlayer videoId={youtubeVideoId} />
         <TeamSection/>
         <CTASection />
